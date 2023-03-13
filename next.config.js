5940f9bc42d9fa069f2cb1b587df86a6044c3ce1
Frank Collins <fwc3rd@gmail.com>
2023-03-13 15:41:29 -0400

    fix issue #6 [ undefined | definition ] err [3:41pm]

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    ALLpokemonURL: `https://pokeapi.co/api/v2/pokemon?limit=300`
+  }
 }
 
 module.exports = nextConfig
+
+// module.exports = {
+//   env: {
+//     tv: 'tv dinner',
+//     dvd: 'dvd dinner'
+//   },
+// }
