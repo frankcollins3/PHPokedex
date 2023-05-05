cca3e3d32b4e990ad0db451381644e178cfa2f7e
Frank Collins <fwc3rd@gmail.com>
2023-05-05 10:03:49 -0400

    fix issue #11 [10:03am]

--- frontend/pokedex/server.js
+++ frontend/pokedex/server.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import expressGraphQL from 'express-graphql'
+// expressGraphQL.graphqlHTTP()
+
+console.log('expressGraphQL log:')
+console.log(expressGraphQL)
+
 const app = express()
 
+app.use('/graphql', expressGraphQL.graphqlHTTP({
+    // schema: schema,
+    graphiql: true
+}))
+
+
 app.listen(3333, () => console.log('listen to server; server listen to you'));
+
