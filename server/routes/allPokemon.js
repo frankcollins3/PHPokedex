a94353b235a56aa4b6c3c96b0ac73011a357e14f
Frank Collins <fwc3rd@gmail.com>
2023-05-07 18:55:54 -0400

    http route to hit all pokemon [6:55pm]

--- server/routes/allPokemon.js
+++ server/routes/allPokemon.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 const allPokemon = async (req, res) => {
+// this route invokes axios' get() -> return endpoint of predata.data.results which is array of original 151 pokemon
     try {
         let predata = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
         let data = predata.data.results || []
