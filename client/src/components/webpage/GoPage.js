f43c73f4689255024d7fa6543595b0265153ddf8
Frank Collins <fwc3rd@gmail.com>
2023-05-11 16:21:27 -0400

    test fetch GraphQL return data setState(data) [4:20pm]

--- client/src/components/webpage/GoPage.js
+++ client/src/components/webpage/GoPage.js
@@ -4,30 +4,39 @@ import PokeballRow from '../elements/PokeballRow'
 import allAPI from '../../utility/allAPI'
 import nameOrIdForData from '../../utility/nameOrIdForData'
 import axios from 'axios'
+import {useEffect, useState} from "react"
 
 
-const firefunc = async () => {
 
+function GoPage () {
+
+  useEffect( () => {
+    console.log("a simple sueEffect test!")
+  }, [])  
+
+  const [pokemon, setPokemon] = useState([])
+
+  const allDataAllPokemonFetch = async () => {
     // (name:"slowpoke") is the resolve: (parent, args) => it is the args that becomes the resolve function parameters in RootQueryType server/index.js
     //     /pokemon is from app.use('pokemon', ExpressGraphQL({}))   /query={allDataAllPokemon} is the key with the resolve function RootQueryType
-    const response = await axios.post('http://localhost:5000/pokemon?query={allDataAllPokemon(id:103){name,poke_id,type,moves,abilities}}');
-    console.log('response')
+    const response = await axios.post('http://localhost:5000/pokemon?query={allDataAllPokemon(id:59){name,poke_id,type,moves,abilities}}');
+    console.log('response') 
     console.log(response)
-
-    for (let i = 0; i < 151; i++) { // c for (int i = 0; i < sizeof(*pokemon); i++) {}
+    for (let i = 170; i < 200; i++) { 
       const loopdata = await axios.post(`http://localhost:5000/pokemon?query={allDataAllPokemon(id:${i+1}){name,poke_id,type,moves,abilities}}`)
-      console.log('loopdata')
-      console.log(loopdata)
+      let data = loopdata.data.data.allDataAllPokemon
+      setPokemon(pokemon => [...pokemon, data])      
+      console.log('data in the for loop')
+      console.log(data)
     }
     return response.data ? response.data : []
   }
 
   const firefunc2 = async () => {
-    console.log("* * * * firefunc2 * * * * ")
-    const response = await fetch('http://localhost:5000/pokemon?query={allAPIpokemon{name}}');
-    // const response = await fetch('http://localhost:5000/pokemon?query={allpokemon{name}}');
-    const data = await response.json();
-    console.log(data);
+     // const response = await fetch('http://localhost:5000/pokemon?query={allAPIpokemon{name}}');
+    // // const response = await fetch('http://localhost:5000/pokemon?query={allpokemon{name}}');
+    // const data = await response.json();
+    // console.log(data);
   }
 
   const apitest = async () => {
@@ -38,7 +47,12 @@ const firefunc = async () => {
     console.log(data)
   }
 
-function GoPage () {
+  const getstate = () => {
+    console.log('pokemon this is the setState()')
+    console.log(pokemon)
+  }
+
+
     const AppClass = ["App", "flex", "column", "justCenterAlignCenter"].join(" ");
 
     return (
@@ -46,9 +60,11 @@ function GoPage () {
 
         
         <RealScreen/>
-        <button style={{backgroundImage: `url('img/pokeball.png')`}}  className="Intro-Buttons Bg-Cover-noRepeat" id="IB1" onClick={firefunc}> </button>
+<button style={{backgroundImage: `url('img/pokeball.png')`}}  className="Intro-Buttons Bg-Cover-noRepeat" id="IB1" onClick={allDataAllPokemonFetch}> </button>
         <button style={{backgroundImage: `url('img/greatball.png')`}} className="Intro-Buttons Bg-Cover-noRepeat" id="IB2" onClick={firefunc2}></button>
         <button style={{backgroundImage: `url('img/ultraball.png')`}} className="Intro-Buttons Bg-Cover-noRepeat" id="IB3" onClick={apitest}></button>
+        <button style={{backgroundImage: `url('img/gear.png')`}} className="Intro-Buttons Bg-Cover-noRepeat" id="IB3" onClick={getstate}></button>
+
 
         {/* <PokeballRow/> */}
         
