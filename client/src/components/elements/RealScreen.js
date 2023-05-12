ede25880ac4ac3a8f0945766c3cdf0d84c4b9760
Frank Collins <fwc3rd@gmail.com>
2023-05-12 04:00:49 -0400

    bankquery.allDataAllPokemon() [4:00am]
    this works

--- client/src/components/elements/RealScreen.js
+++ client/src/components/elements/RealScreen.js
@@ -2,26 +2,29 @@ import '../../App.css';
 import {useEffect, useState} from 'react'
 import store from "../../redux/store"
 import actionObject from "../../redux/actions.js"
+import CentralQuery from "../../utility/GraphQL/CentralQuery"
 
 const firefunc = () => { return }
 const firefunc2 = () => { return }
 
+let globalvar;
+let pokemon;
+let querybank;
+
 const apitest = async () => { 
+    console.log('querybank')
+    console.log(querybank)
+
     console.log('apitest');
-    // let predata = await fetch(`http://localhost:5000/pokemon?query={allDataAllPokemon{name,poke_id}}`)
-    let predata_allAPI = await fetch(`http://localhost:5000/pokemon?query={allAPIpokemon{name,poke_id}}`)
+    let predata_allAPI = await fetch(querybank.allAPIpokemon)
+    
     let predata = await predata_allAPI.json()
     let allAPI = predata.data.allAPIpokemon
     
     allAPI.forEach(async(pokemon) => {
-
-        console.log('pokemon')
-        console.log(pokemon)
-
-          let predata_allDataAllPoke = await fetch(`http://localhost:5000/pokemon?query={allDataAllPokemon(name:"${pokemon.name}"){name,poke_id}}`)
+          let predata_allDataAllPoke = await fetch(`http://localhost:5000/pokemon?query={allDataAllPokemon(name:"${pokemon.name}"){name,poke_id,type,moves,abilities,image}}`)
           // let predata_allDataAllPoke = await fetch(`http://localhost:5000/pokemon?query={allDataAllPokemon(name:${pokemon.name}){name,poke_id}}`)
           let allDataAllPoke = await predata_allDataAllPoke.json()
-          console.log('allDataAllPoke')
           console.log(allDataAllPoke)
     })
 
@@ -37,22 +40,28 @@ function RealScreen () {
 
     const [pokeName, setPokeName] = useState([])
 
-    let globalvar
-    let pokemon;
-
     const getstate = async () => {          
       let slowpoke = await actionObject.slowpoke()
-      let predata = await fetch(`http://localhost:5000/pokemon?query={allAPIpokemon{name,poke_id}}`)
-      const allAPIpokemon = await predata.json()
-      const allpokemon = allAPIpokemon.data.allAPIpokemon
-    
-      const pokemonPromises = allpokemon.map(async (pokemon) => {
-        const reduxname = await actionObject.setPokemon(pokemon.name).payload
-        return reduxname
-      })
-    
-      const pokemonNames = await Promise.all(pokemonPromises)
-      if (pokeName.length < 151) { setPokeName(pokeName => [...pokeName, ...pokemonNames]) }
+
+      // let predata = await fetch(querybank.allDataAllPokemon()) // wrong.
+      let predata = await querybank.allDataAllPokemon()
+      console.log('predata')
+      console.log(predata)
+      
+
+      // let predata = await fetch(querybank.allAPIpokemon)
+      // const allAPIpokemon = await predata.json()
+      // const allpokemon = allAPIpokemon.data.allAPIpokemon
+
+      // allpokemon.forEach( (pokemon) => {
+      //   const name = pokemon.name
+      //   console.log('name')
+      //   console.log(name)
+      //   // setPokeName(pokeName => [pokeName, name])
+      //   // setPoke ame(pokeName => [...pokeName, {name: name, image: image}])
+      //   setPokeName(pokeName => [...pokeName, {name:pokemon.name}])
+      // })
+      
     }
 
     const state2 = () => {
@@ -64,34 +73,45 @@ function RealScreen () {
     useEffect( () => {
       (async() => {
         globalvar = await store.getState();
+        querybank = await CentralQuery()
         pokemon = globalvar.pokemon
       })()
 
     }, [])
+
     
 
 
     return (
       <>
     <div className={AppClass}>        
-    <div className="Screen" style={{ backgroundImage: `url('img/nicePokedex.png')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
+    <div className="Screen" 
+    style={{ 
+      backgroundImage: `url('img/nicePokedex.png')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'
+    }}>
+            {/* <ul className="Screen-List"> */}
             {
               pokeName.length 
               ?
+
+              
               pokeName.map( (mapitem, index) => {
-                  return (
-                      <h6 key={index}> {mapitem} </h6>    
+                console.log('mapitem')
+                console.log(mapitem)
+                return (
+                  <div key={`div:${index}`} className={flexclasscolumn}>
+                  <li className="Pokemon-Screen-Data" key={index}> {mapitem.name} </li>    
+                  <img src={mapitem.image} key={`image: ${index}`} />
+                  </div>
+                  
                   )
-              })
-              :
-              <img className="gear" src="img/gear.png"></img>              
-            }
-            {/* {
-            pokemon.length > 3 ? 
-              pokemon || 'no pokemon'
-              :
-              <img className="gear" src="img/gear.png"></img>
-            } */}
+                })
+                
+                :
+                <img className="gear" src="img/gear.png"></img>              
+              }
+              {/* </ul> */}
+            
        </div>
     <button style={{backgroundImage: `url('img/pokeball.png')`}} className="Intro-Buttons Bg-Cover-noRepeat" id="IB2" onClick={firefunc2}></button>
         <button style={{backgroundImage: `url('img/greatball.png')`}} className="Intro-Buttons Bg-Cover-noRepeat" id="IB3" onClick={apitest}></button>
