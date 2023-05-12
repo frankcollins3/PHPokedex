10b1402d9a80f2a45c0d01cf95cf8089be97ff4a
Frank Collins <fwc3rd@gmail.com>
2023-05-12 15:53:30 -0400

    fix issue #45 arr[!data] Promise.resolve [3:48pm]

--- client/src/utility/GraphQL/allDataAllPokemonFunc.js
+++ client/src/utility/GraphQL/allDataAllPokemonFunc.js
@@ -1,16 +1,37 @@
+
 export default async function allDataAllPokemonFunc () {
   let predata_allAPI = await fetch(`http://localhost:5000/pokemon?query={allAPIpokemon{name,poke_id}}`)
-    let predata = await predata_allAPI.json()
-    let allAPI = predata.data.allAPIpokemon
-    let bucket = [];    
-      await allAPI.forEach(async(pokemon) => {
-            let predata_allDataAllPoke = await fetch(`http://localhost:5000/pokemon?query={allDataAllPokemon(name:"${pokemon.name}"){name,poke_id,type,moves,abilities,image}}`)
-            // let predata_allDataAllPoke = await fetch(`http://localhost:5000/pokemon?query={allDataAllPokemon(name:${pokemon.name}){name,poke_id}}`)
-            let allDataAllPoke = await predata_allDataAllPoke.json()            
-            allDataAllPoke = allDataAllPoke.data.allDataAllPokemon
-            console.log('allDataAllPoke')
-            console.log(allDataAllPoke)
-              await bucket.push(allDataAllPoke)                    
-      })
-    return bucket
+  let predata = await predata_allAPI.json()
+  let allAPI = predata.data.allAPIpokemon
+  let bucket = [];    
+
+const loopAndPush = async () => {
+  await Promise.all(allAPI.map(async (pokemon) => {
+    let predata_allDataAllPoke = await fetch(`http://localhost:5000/pokemon?query={allDataAllPokemon(name:"${pokemon.name}"){name,poke_id,type,moves,abilities,image}}`)
+    let allDataAllPoke = await predata_allDataAllPoke.json()            
+    allDataAllPoke = allDataAllPoke.data.allDataAllPokemon
+    
+    let myobject = {
+      name: allDataAllPoke.name, poke_id: allDataAllPoke.poke_id,  image: allDataAllPoke.image, moves: allDataAllPoke.moves,
+      abilities: allDataAllPoke.abilities, type: allDataAllPoke.type
+    }
+
+    bucket.push(myobject);                    
+  }));
+}
+
+    const returnbucket = async () => {return await bucket}
+
+    const pushAndReturn = async () => {
+      await loopAndPush()
+      console.log('bucket')
+      console.log(bucket)
+      console.log(bucket[0])
+      // console.log(bucket[1])
+      return returnbucket()
+    }
+    return pushAndReturn()
+
+      
+    // return bucket
 }
